f11ad1be74850e5ed859d4c7c4470f2426ac226f
Maciej Szulik <maszulik@redhat.com>
2019-08-26 15:28:44 +0200

    UPSTREAM: <carry>: kubectl: printers

--- pkg/printers/patch_sourcechecker.go
+++ pkg/printers/patch_sourcechecker.go
@@ -2,17 +2,16 @@ package printers
 
 func init() {
 	disallowedPackagePrefixes = append(disallowedPackagePrefixes,
-		"github.com/openshift/origin/pkg/apps/apis/",
-		"github.com/openshift/origin/pkg/authorization/apis/",
-		"github.com/openshift/origin/pkg/build/apis/",
-		"github.com/openshift/origin/pkg/image/apis/",
-		"github.com/openshift/origin/pkg/network/apis/",
-		"github.com/openshift/origin/pkg/oauth/apis/",
-		"github.com/openshift/origin/pkg/project/apis/",
-		"github.com/openshift/origin/pkg/quota/apis/",
-		"github.com/openshift/origin/pkg/route/apis/",
-		"github.com/openshift/origin/pkg/security/apis/",
-		"github.com/openshift/origin/pkg/template/apis/",
-		"github.com/openshift/origin/pkg/user/apis/",
+		"github.com/openshift/openshift-apiserver/pkg/apps/apis/",
+		"github.com/openshift/openshift-apiserver/pkg/authorization/apis/",
+		"github.com/openshift/openshift-apiserver/pkg/build/apis/",
+		"github.com/openshift/openshift-apiserver/pkg/image/apis/",
+		"github.com/openshift/openshift-apiserver/pkg/oauth/apis/",
+		"github.com/openshift/openshift-apiserver/pkg/project/apis/",
+		"github.com/openshift/openshift-apiserver/pkg/quota/apis/",
+		"github.com/openshift/openshift-apiserver/pkg/route/apis/",
+		"github.com/openshift/openshift-apiserver/pkg/security/apis/",
+		"github.com/openshift/openshift-apiserver/pkg/template/apis/",
+		"github.com/openshift/openshift-apiserver/pkg/user/apis/",
 	)
 }
